c50a1939566c6b9bc51f201b41f40c0146b49de3
dobnowqa <45106777+dobnowqa@users.noreply.github.com>
2019-01-15 13:57:56 -0500

    latest fab4 files as of 2019-01-15
    latest fab4 files as of 2019-01-15  - SfShFnNew

--- src/com/fab4/SfShFnNew.java
+++ src/com/fab4/SfShFnNew.java
@@ -5,7 +5,6 @@ import com.util.TestUtil;
 import com.util.Xls_Reader;
 import java.util.Hashtable;
 
-import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
@@ -19,16 +18,18 @@ import com.base.TestBase;
 import com.pages.DobDashboardPage;
 import com.pages.DobDocumentsPage;
 import com.pages.DobPW1Page;
-import com.pages.DobSOWPage;
 import com.pages.DobPW3Page;
+import com.pages.DobSOWPage;
 import com.pages.DobTR1Page;
 import com.pages.DobTR8Page;
 import com.pages.DobSignaturesPage;
 import com.relevantcodes.extentreports.LogStatus;
 
 public class SfShFnNew extends TestBase {
-	
-	String testname = "SfShFnNew";
+    // This test case uses DOBNOW to create an application/job/filing for new-work for a combination of Scaffold (SF), Sidewalk Shed (SH) and Fence (FN).
+	// This test case needs to run with config.properties environment = "fab4"
+	String testname = this.getClass().getSimpleName();
+	// The following file is used for FN, SF, SG and SH work types:
 	Xls_Reader xlsx = new Xls_Reader(Constants.testCasesFab4);
 	
 	@BeforeSuite
@@ -57,43 +58,52 @@ public class SfShFnNew extends TestBase {
 		return TestUtil.getData(testname, xlsx);
 	}
 
+	// Execute the Base test, using the data defined above, to create the number of jobs equal to invocationCount.
 	@Test(dataProvider = "getTestData",invocationCount = 1)
 	public void Base(Hashtable<String, String> data) {
 		if (!TestUtil.isExecutable(testname, xlsx) || data.get("Runmode").equals("N"))
 			throw new SkipException("Skipping test");
-		System.out.println("BEGIN " + convertedTimestamp() + " **************** " + data.get("description"));
+		System.out.println(convertedTimestamp() + " ******BEGIN***** " + testname + ": " + data.get("description") + " " + env);
 		test = rep.startTest(data.get("description"));
 		test.log(LogStatus.INFO, data.get("description"));
 		test = rep.startTest("Test Case Data");
 		test.log(LogStatus.INFO, data.toString());
-		DobDashboardPage dash = PageFactory.initElements(driver, DobDashboardPage.class);
-		DobPW1Page pw1 = PageFactory.initElements(driver, DobPW1Page.class);
-		DobSOWPage asw = PageFactory.initElements(driver, DobSOWPage.class);
-//		DobPW3Page pw3 = PageFactory.initElements(driver, DobPW3Page.class);
-		DobTR1Page tr1 = PageFactory.initElements(driver, DobTR1Page.class);
-//		DobTR8Page tr8 = PageFactory.initElements(driver, DobTR8Page.class);
-		DobSignaturesPage signature = PageFactory.initElements(driver, DobSignaturesPage.class);
-		DobDocumentsPage docs = PageFactory.initElements(driver, DobDocumentsPage.class);
+		DobDashboardPage 	dash = PageFactory.initElements(driver, DobDashboardPage.class);
+		DobPW1Page 			pw1  = PageFactory.initElements(driver, DobPW1Page.class);
+		DobSOWPage 			asw  = PageFactory.initElements(driver, DobSOWPage.class);
+		DobPW3Page 			pw3  = PageFactory.initElements(driver, DobPW3Page.class);
+		DobTR1Page 			tr1  = PageFactory.initElements(driver, DobTR1Page.class);
+		DobTR8Page 			tr8  = PageFactory.initElements(driver, DobTR8Page.class);
+		DobSignaturesPage 	signature = PageFactory.initElements(driver, DobSignaturesPage.class);
+		DobDocumentsPage 	docs = PageFactory.initElements(driver, DobDocumentsPage.class);
 
-		dash.selectWorkType(data.get("work_type"));
-		pw1.locationImfo(data.get("address"));
-		type(Constants.pw1_1_apt_suite_number, testname);
-		pw1.workOnFloors(data.get("work_on_floors"));
-		pw1.applicantInfo(data.get("user_info"));
-		pw1.reviewtype(data.get("filing_review_type"));
-		pw1.directive14acceptanceRequested(data.get("directive_14"));
-		pw1.workTypeScaffold(data.get("new_existing_both")); // workTypeScaffold
-		pw1.additionalInfo2(data.get("additional_info"));
-		pw1.additionalConciderations(data.get("additional_conciderations"));
-//		pw1.complianceNYCECC(data.get("nycecc"));
-		pw1.zonningCharacteristics(data.get("zonning"));
-		pw1.buildingCharacteristics(data.get("building_charcteristics"));
+		dash.selectJobFilingWorkType(data.get("work_type"));		
+//		pw1.enterFilingInformation(data.get("description")); // no filing info for this worktype
+		pw1.enterLocationInformation(data.get("address"));
+		pw1.addWorkOnFloors(data.get("work_on_floors"));
+		pw1.enterApplicantInformation(data.get("user_info"));
+		pw1.enterWorkTypes(data.get("new_existing_both"));
+		pw1.enterAdditionalInformation(data.get("additional_info"));
+		pw1.enterNYCECCCompliance(data.get("nycecc"));
+		pw1.enterJobDescription();
+		pw1.enterSiteCharacteristics(data.get("site_characteristics"));
+		pw1.enterAsbestosAbatementCompliance(data.get("site_characteristics"));
 		pw1.constructionEquipment(data.get("equipment"));
-		pw1.fireProtectionEquipment2(data.get("fire_equipment"));
-		pw1.siteCharacteristics(data.get("site_characteristics"));
-		pw1.savePW1(data.get("save_pw1"));
-		asw.scopeOfWorkScaffold(data.get("asw"));
-//		tr1.progressInspection(data.get("tr1")); // temp not in use
+		pw1.enterPw1Comments(data.get("description"));	
+		pw1.saveJob("get_number");
+		pw1.enterAdditionalConsiderations(data.get("additional_conciderations"));
+		pw1.enterBuildingCharacteristics(data.get("building_charcteristics")); // Zoning Info
+		pw1.saveJob("skip_number");
+		
+		asw.enterScopeOfWorkConstructionFence(data.get("asw"));
+		asw.enterScopeOfWorkSidewalkShed(data.get("asw"));
+		asw.enterScopeOfWorkSupportedScaffold(data.get("asw"));
+				
+		pw3.addCostAffidavit(data.get("pw3"));
+				
+		tr1.addSpecialInspection(data.get("tr1"));
+		tr1.enterProgressInspectionSignature(data.get("tr1"));
+		
 		signature.applicantStatementsSignature(data.get("signatures"));
 		signature.ownerSignature(data.get("owner_signature"));
 		docs.uploadDocuments(data.get("documents"));
